20b1ee8e09f2c33e34cc1daacbf1c73de1469b33
LeonardoPerin97 <140596442+LeonardoPerin97@users.noreply.github.com>
2024-10-19 02:37:04 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import create_app, db
+import os
 
 
 app = create_app()
@@ -24,4 +25,7 @@ with app.app_context():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Usa la variabile d'ambiente PORT o la porta di default 5000
+    port = int(os.environ.get('PORT', 5000))
+    # Esegui l'app (senza debug in produzione)
+    app.run(host='0.0.0.0', port=port, debug=os.environ.get('FLASK_DEBUG', False) == '1')
